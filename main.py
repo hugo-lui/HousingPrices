f39170e203f53d4b19dfe3020b1859498e84e602
Hugo Lui <115033116+hugo-lui@users.noreply.github.com>
2024-01-25 00:50:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
--- model.py
+++ model.py
@@ -1,15 +1,19 @@
 from keras.models import Sequential
-from keras.layers import InputLayer
 from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import InputLayer
 from keras.optimizers import Adam
 
 def design_model(features, learning_rate):
-    model = Sequential(name = "my_first_model")
+    model = Sequential(name = "model")
     inputLayer = InputLayer(input_shape=(features.shape[1],)) 
     model.add(inputLayer) 
     model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.1))
     model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(24, activation='relu'))
+    model.add(Dropout(0.3))
     model.add(Dense(1)) 
     opt = Adam(learning_rate=learning_rate)
     model.compile(loss='mse',  metrics=['mae'], optimizer=opt)
